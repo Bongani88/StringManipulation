a576e9440ab334ccade4806a97cd3676b66dfd45
Bongani88 <149568948+Bongani88@users.noreply.github.com>
2023-11-10 08:33:08 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Threading.Channels;
+
+
+//AUTHOR: BONGANI MAGWA
+namespace CastingCode
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.Write("Enter your name: ");
+            String name = Console.ReadLine();
+            foreach (char character in name)
+            {
+                if (char.IsLetter(character))
+                {
+                    Console.WriteLine("proceede");
+                } else if (char.IsDigit(character))
+                {
+                    Console.WriteLine("invalid name: " + character);
+                }
+
+            }
+            Console.Write("Enter Your years of experience: ");
+            
+            int expwerience  = Convert.ToInt32(Console.ReadLine());
+            if(expwerience > 5)
+            {
+                Console.WriteLine("START YOUR SIDE HUSTLE");
+            } else
+            {
+                Console.WriteLine("KEEP WORKING HARD!!!!!");
+            }
+            Console.WriteLine("Hi " + name + " you're " + expwerience + " OF EXPERIENCE GREATE WORK BRO!");
+            Console.ReadLine();
+        }
+    }
+}
